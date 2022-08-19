b2ee550588adc5ebcbaff7020d86954da171881a
Yves NJAMEN <njamenconsulting@gmail.com>
2022-08-19 13:20:58 +0200

    scheduler job

--- app/Repositories/TrenzRepository.php
+++ app/Repositories/TrenzRepository.php
@@ -11,24 +11,50 @@ Class TrenzRepository
     
     //
     public function getArticle(){
-        $url = self::BASE_URL."api/articles";
-        //$response = Http::withOptions(['verify' => public_path('certs/cacert.pem')])->withDigestAuth('mirifica-api', 'p0pdsCqGWQDnReEy85KnK8yCCAwRQpEhn6nNSCom')
-        //                                                                            ->get($url);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_USERPWD, "mirifica-api:p0pdsCqGWQDnReEy85KnK8yCCAwRQpEhn6nNSCom");
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-        $response = curl_exec($ch);
+        $url = self::BASE_URL."api/articles?language=2";
+        /* 
+        $response = Http::withOptions([ 'verify' => public_path('certs/cacert.pem'),
+                                       'Content-Type'=> 'application/json'])
+                    ->withDigestAuth('mirifica-api', 'p0pdsCqGWQDnReEy85KnK8yCCAwRQpEhn6nNSCom')
+                    ->get($url);
+                    */
+                                                                                  
+        $certificate_location = public_path('certs/cacert.pem');
+        $curl = curl_init();
 
-        // validate HTTP status code (user/password credential issues)
-        $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        if ($status_code != 200) echo $status_code;
-
-        echo $response;
-        dd($status_code);
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => 'https://www.trenz-electronic.de/api/articles?language=2',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_SSL_VERIFYHOST=> $certificate_location,
+          CURLOPT_SSL_VERIFYPEER=> $certificate_location,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'GET',
+          CURLOPT_POSTFIELDS =>'{
+            "filter": [
+                {
+                    "property": "active",
+                    "value": 1
+                }
+            ]
+        }',
+          CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json',
+            'Authorization: Basic bWlyaWZpY2EtYXBpOnAwcGRzQ3FHV1FEblJlRXk4NUtuSzh5Q0NBd1JRcEVobjZuTlNDb20='
+          ),
+        ));
+        
+        //Execute cURL and return the string. depending on your resource this returns output like
+        if(curl_exec($curl) === false) 
+           dd('Erreur Curl : ' . curl_error($curl));
+        else $response = curl_exec($curl);
+        
+        curl_close($curl);
+        
+        dd($response);
         
         return $response;
     }
